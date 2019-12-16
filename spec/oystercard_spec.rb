0513ee0ae40d5f46c9f8a07b395f1b36d0858a50
Vish Mayer <vish.mayer@gmail.com>
2019-12-16 15:29:29 +0000

    created balance and top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,16 @@
-describe 'oystercard' do
+require 'oystercard'
+
+describe Oystercard do
+
+  describe '#initialize' do
+    it "has a balance of 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
+  describe '#top_up' do
+    it "allows user to top up balance" do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+  end
 end
