b0545aeded86dc83bac03fcb88fca7188448b98b
Umberto Valente <uvalente89@gmail.com>
2019-12-16 17:38:09 +0000

    Oyster card needs a minimum balance to travel

--- lib/barrier.rb
+++ lib/barrier.rb
@@ -1,8 +1,17 @@
 class Barrier
+  MINIMUM_BALANCE = 1
+
   def touch_in(card)
+    raise 'Not enough funds. Please top up.' if minimum_balance?(card)
+    
     card.touch_in
   end
+
   def touch_out(card)
     card.touch_out
   end 
+
+  def minimum_balance?(card)
+    card.balance < MINIMUM_BALANCE
+  end
 end
--- spec/barrier_spec.rb
+++ spec/barrier_spec.rb
@@ -1,18 +1,30 @@
 require 'barrier'
 
 describe Barrier do
+  let(:card) { Oystercard.new }
+  context 'with positive balance' do
+    before(:each) do
+      card.top_up(10)
+    end
 
-  describe '#touch_in' do
-    it 'to change journey status of the card to true' do
-      card = Oystercard.new
-      expect(subject.touch_in(card)).to eq true
+    describe '#touch_in' do
+      it 'to change journey status of the card to true' do
+        expect(subject.touch_in(card)).to eq true
+      end
+    end
+
+    describe '#touch_out' do
+      it "to change journey status of the card to false" do
+        subject.touch_in(card)
+        expect(subject.touch_out(card)).to eq false
+      end
     end
   end
-  describe '#touch_out' do
-    it "to change journey status of the card to false" do
-      card = Oystercard.new
-      subject.touch_in(card)
-      expect(subject.touch_out(card)).to eq false
+
+  context 'with 0 balance' do
+    it 'raise an error if there\'s no minimum balance' do
+      message = 'Not enough funds. Please top up.'
+      expect { subject.touch_in(card) }.to raise_error message
     end
   end
 end
